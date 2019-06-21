e8a3676cff15d4caee66b1de44eb164ef531a1f0
York Huang <york.huang@honeywell.com>
2019-06-21 19:31:07 +1000

    cleaned up

--- src/engine.js
+++ src/engine.js
@@ -1,32 +1,38 @@
-
-import directions from './directions';
+import directions from "./directions";
 const width = 96;
 const height = 68;
-//snake::Array Array Number
-//food::Array Number
-//state:: {snake, food, isAlive, previousDirection, direction}
-const init = state=>({
-    snake: [[width/2, height/2]]
+//Point:: [Number, Number]
+//state:: {snake: Array Point, food: Point, isAlive: Boolean, previousDirection: Number, direction: Number}
+const init = () => ({
+  snake: [[width / 2, height / 2]]
 });
 
-const nextMoveMapper = (direction, previousDirection)=>{
-    return {
-        [directions.up]: point=>[point[0], point[1] - 1],
-        [directions.down]: point=>[point[0], point[1] + 1],
-        [directions.left]: point=>[point[0]  - 1, point[1]],
-        [directions.right]: point=>[point[0] + 1, point[1]],
-    };
-}
+const nextMoveMapper = {
+  [directions.up]: point => [point[0], point[1] - 1],
+  [directions.down]: point => [point[0], point[1] + 1],
+  [directions.left]: point => [point[0] - 1, point[1]],
+  [directions.right]: point => [point[0] + 1, point[1]]
+};
 
-const move = state=>{
-    const isOppositeDirection = Math.abs(state.direction - state.previousDirection) === 2;
-    const mapper = nextMoveMapper(state.direction, state.previousDirection)[isOppositeDirection ? state.previousDirection : state.direction];
-        return {
-            snake : state.snake.map(mapper)
-        };
-}
+const isOppositeDirection = (keycodeA, keycodeB) =>
+  Math.abs(keycodeA - keycodeB) === 2;
 
-const next = (state)=>state.snake.length === 0 ? init(state): move(state);
+const getEffectiveDirection = (previousDirection, direction) =>
+  isOppositeDirection(previousDirection, direction)
+    ? previousDirection
+    : direction;
 
+const move = state => {
+  const effectiveDirection = getEffectiveDirection(
+    state.previousDirection,
+    state.direction
+  );
+  const mapper = nextMoveMapper[effectiveDirection];
+  return {
+    snake: state.snake.map(mapper)
+  };
+};
+
+const next = state => (state.snake.length === 0 ? init(state) : move(state));
 
 export default next;
